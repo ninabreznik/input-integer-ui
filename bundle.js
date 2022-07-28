f005b5e760dcdaeaac17d79e591a843e2bff5bc5
Nina Breznik <ninabreznik@gmail.com>
2022-07-28 01:12:40 +0100

    Update demo

--- bundle.js
+++ bundle.js
@@ -1,9 +1,25 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const inputInteger = require('..')
 
-const x = inputInteger()
+const input1 = inputInteger()
+const input2 = inputInteger()
 
-document.body.append(x)
+const title = 'My demo form'
+const subtitle = 'Please fill out the form'
+
+const page = document.createElement('div')
+page.innerHTML = `
+  <h1> ${title} </h1>
+  <h2> ${subtitle} </h2>
+  <h3>Enter your age</h3>
+  <x></x>
+  <h3>Enter your year of birth</h3>
+  <y></y>
+`
+page.querySelector('x').replaceWith(input1)
+page.querySelector('y').replaceWith(input2)
+
+document.body.append(page)
 },{"..":2}],2:[function(require,module,exports){
 module.exports = inputInteger
 
@@ -28,42 +44,42 @@ function inputInteger () {
 
 function get_theme () {
   return `
-  :host {
-    --b: 0, 0%;
-    --color-white: var(--b), 100%;
-    --color-black: var(--b), 0%;
-    --color-grey: var(--b), 85%;
-    --bg-color: var(--color-grey);
-    --shadow-xy: 0 0;
-    --shadow-blur: 8px;
-    --shadow-color: var(--color-white);
-    --shadow-opacity: 0;
-    --shadow-opacity-focus: 0.65;
-  }
-  input {
-    text-align: left;
-    align-items: center;
-    font-size: 1.4rem;
-    font-weight: 200;
-    color: hsla(var(--color-black), 1);
-    background-color: hsla(var(--bg-color), 1);
-    padding: 8px 12px;
-    box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
-    transition: font-size .3s, color .3s, background-color .3s, box-shadow .3s ease-in-out;
-    outline: none;
-    border: 1px solid hsla(var(--bg-color), 1);
-    border-radius: 8px;
-  }
-  input:focus {
-    --shadow-color: var(--color-black);
-    --shadow-opacity: var(--shadow-opacity-focus);
-    --shadow-xy: 4px 4px;
-    box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
-  }
-  input::-webkit-outer-spin-button, 
-  input::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-  }
+    :host {
+      --b: 0, 0%;
+      --color-white: var(--b), 100%;
+      --color-black: var(--b), 0%;
+      --color-grey: var(--b), 85%;
+      --bg-color: var(--color-grey);
+      --shadow-xy: 0 0;
+      --shadow-blur: 8px;
+      --shadow-color: var(--color-white);
+      --shadow-opacity: 0;
+      --shadow-opacity-focus: 0.65;
+    }
+    input {
+      text-align: left;
+      align-items: center;
+      font-size: 1.4rem;
+      font-weight: 200;
+      color: hsla(var(--color-black), 1);
+      background-color: hsla(var(--bg-color), 1);
+      padding: 8px 12px;
+      box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+      transition: font-size .3s, color .3s, background-color .3s, box-shadow .3s ease-in-out;
+      outline: none;
+      border: 1px solid hsla(var(--bg-color), 1);
+      border-radius: 8px;
+    }
+    input:focus {
+      --shadow-color: var(--color-black);
+      --shadow-opacity: var(--shadow-opacity-focus);
+      --shadow-xy: 4px 4px;
+      box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+    }
+    input::-webkit-outer-spin-button, 
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+    }
   `
 }
 
--- src/index.js
+++ src/index.js
@@ -21,42 +21,42 @@ function inputInteger () {
 
 function get_theme () {
   return `
-  :host {
-    --b: 0, 0%;
-    --color-white: var(--b), 100%;
-    --color-black: var(--b), 0%;
-    --color-grey: var(--b), 85%;
-    --bg-color: var(--color-grey);
-    --shadow-xy: 0 0;
-    --shadow-blur: 8px;
-    --shadow-color: var(--color-white);
-    --shadow-opacity: 0;
-    --shadow-opacity-focus: 0.65;
-  }
-  input {
-    text-align: left;
-    align-items: center;
-    font-size: 1.4rem;
-    font-weight: 200;
-    color: hsla(var(--color-black), 1);
-    background-color: hsla(var(--bg-color), 1);
-    padding: 8px 12px;
-    box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
-    transition: font-size .3s, color .3s, background-color .3s, box-shadow .3s ease-in-out;
-    outline: none;
-    border: 1px solid hsla(var(--bg-color), 1);
-    border-radius: 8px;
-  }
-  input:focus {
-    --shadow-color: var(--color-black);
-    --shadow-opacity: var(--shadow-opacity-focus);
-    --shadow-xy: 4px 4px;
-    box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
-  }
-  input::-webkit-outer-spin-button, 
-  input::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-  }
+    :host {
+      --b: 0, 0%;
+      --color-white: var(--b), 100%;
+      --color-black: var(--b), 0%;
+      --color-grey: var(--b), 85%;
+      --bg-color: var(--color-grey);
+      --shadow-xy: 0 0;
+      --shadow-blur: 8px;
+      --shadow-color: var(--color-white);
+      --shadow-opacity: 0;
+      --shadow-opacity-focus: 0.65;
+    }
+    input {
+      text-align: left;
+      align-items: center;
+      font-size: 1.4rem;
+      font-weight: 200;
+      color: hsla(var(--color-black), 1);
+      background-color: hsla(var(--bg-color), 1);
+      padding: 8px 12px;
+      box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+      transition: font-size .3s, color .3s, background-color .3s, box-shadow .3s ease-in-out;
+      outline: none;
+      border: 1px solid hsla(var(--bg-color), 1);
+      border-radius: 8px;
+    }
+    input:focus {
+      --shadow-color: var(--color-black);
+      --shadow-opacity: var(--shadow-opacity-focus);
+      --shadow-xy: 4px 4px;
+      box-shadow: var(--shadow-xy) var(--shadow-blur) hsla( var(--shadow-color), var(--shadow-opacity));
+    }
+    input::-webkit-outer-spin-button, 
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+    }
   `
 }
 
